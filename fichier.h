c590884f28eb5e402e8e6c5ced15efe7535e88c7
sankavi-k <sankavi.kumanan@gmail.com>
2025-04-30 17:53:02 +0200

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+type
+
 typedef Struct{
 int identifiant;
 char nom[MAX_nom];
 Espece espece;
 int annee;
 char commentaire[MAX_com];
-}Animal;
+} Animal;
 
 
