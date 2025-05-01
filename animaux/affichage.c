a9ba9a30d78f0b413244573706dda2428d8575dd
nisrine-lk <nisrine.lakhmiri@etu.u-cergy.fr>
2025-05-01 21:20:35 +0200

    Update affichage.c

--- animaux/affichage.c
+++ animaux/affichage.c
@@ -1,2 +1,16 @@
-#include <stdio.h>
-#include <stdio
+#include <fichier.h>
+
+void afficher_animal(Animal a ) {
+  printf ("\n-- Fiche Animal --\n");
+  printf ("ID: %d\n" , a.id);
+  printf ("Nom: %s\n" , a.nom);
+  printf ("Espéce: %s\n" , a.espece);
+  printf ("Age: %d ans\n" , 2025 - a.annee_naissance); 
+
+printf("Poids: %.2 kg\n" , a.poids);
+
+if (a.commentaire[0] != 0) { 
+  printf("Coommentaire: %s\n", a.commentaire);
+} else { 
+    printf("Commentaire: Aucun \n" );
+}
