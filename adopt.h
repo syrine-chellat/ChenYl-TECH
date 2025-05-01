36ddc372e8e6f1a6ab59349bf370a7936976c7dc
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-02 00:43:32 +0200

    Update adopt.h

--- adopt.h
+++ adopt.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void adopter_animal(Animal* animaux[], int* nb_animaux) {
+  int identifiant;
+  printf("Entrez le numéro d'identifinat de l'animal à adopter :");
+  scanf("%d",&identifiant);
+
+
 
