a40826e90a6320149eb0a8689ad3e606c777d986
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-02 02:01:55 +0200

    Update recherchanim.c

--- recherchanim.c
+++ recherchanim.c
@@ -1,9 +1,10 @@
 #include <fichier.h>
 
 void rechercher_animal(){
-char nom[50]== NULL;
-
-
+char nom[50]== "";
+int espece=-1;
+int age=-1;
+int choix=0;
 
 
 printf("Voulez vous chercher par nom ? 1=espece 2=non ) :");
@@ -31,22 +32,35 @@ printf("\n------------RESULTAT(S)----------------\n");
 
 int anim_trouvés=0;
 
-for (int=0; i<animaux[i], i++){
+for (int=0; i<animaux, i++){
   int correpondance==1;
 
-  if(strcmp(animaux[i]->nom, nom) !=0){
+  if(strlen(nom)>0 && strcmp(animaux[i]->nom, nom) !=0){
     correspondance==0;
-}
+  }
 
-  if ((animaux[i]->espece, espece
-  if
+  if (espece!=-1 && animaux[i]->espece != espece){
+    correspondance==0;
+  }
+
+  int age_anim=2025 - animaux[i]->annee;
+  if(age == 1 && animaux_age>=2){
+    correspondance==0;
+  }
+  if(age == 2 && animaux_age<=10){
+    correspondance==0;
   }
 
   if(correpondance==1){
+    anim_trouvés++;
+    printf("Identifiant : %d \n Nom : %s \n Espèce : %s \n Année : %d \n Poids : %2f kg \n Commentaires : %s\n", animaux[i].identifiant, animaux.nom, animaux.espece.??, animaux.poids, animaux.commentaire);
   }
-  if (anim_trouvés==0){
+}
+printf( "Nombre d'animaux trouvés : %d \n",anim_trouvés);
+}
+
+if (anim_trouvés==0){
   printf("Aucun animal trouvés");
-  }
 }
 // revenir à l'acceuil ??
 
