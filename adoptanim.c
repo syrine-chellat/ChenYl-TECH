b8b6e4808a856130de5fdad53321eda238b6b28f
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-02 02:44:37 +0200

    Update adoptanim.c

--- adoptanim.c
+++ adoptanim.c
@@ -1,9 +1,37 @@
 #include <fichier.h>
 
-void adopter_animal(Animal* animaux[], int* nb_animaux) {
+void adopter_animal(Animal animaux[], int* nombre_animaux) {
   int identifiant;
+  int trouvé_id=0;
   printf("Entrez le numéro d'identifinat de l'animal à adopter :");
   scanf("%d",&identifiant);
+
+for (int i=0; i<animaux[i];i++){
+  if(animaux[i].identifiant==identifiant){
+    
+    printf("Animal trouvé : %s(%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
+    
+    for(int j=i; j<*nombre_animaux-1; j++){
+      animaux[j]=aniamux[j+1];
+    }
+    
+    animaux[nombre_animaux-1].identifiant=0;
+    animaux[nombre_animaux-1].nom[0]="\0";
+    animaux[nombre_animaux-1].commentaire[0]="\0";
+    
+    nombre_animaux-=;
+    printf("L'animal a été adopté. Prenez bien soin de lui.\n");
+    trouvé_id=1;
+  }
+break;
+}
+
+if(trouvé_id!=1){
+  printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n), identifiant);
+}
+  
+}
+
 
 
 
