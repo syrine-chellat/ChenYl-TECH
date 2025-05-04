07469cfd4c17765853bbc8a93f7c3e6b0551f0d6
syrine-chellat <syrine.chellat@gmail.com>
2025-05-04 15:24:03 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -14,3 +14,20 @@ if (a.commentaire[0] != \0) {
 } else { 
     printf("Commentaire: Aucun \n" );
 }
+
+
+void afficher_animal(Animal* a, int esp) {
+    printf ("\n-- Fiche Animal --\n");
+    printf ("ID: %d\n" , a->identifiant);
+    printf ("Nom: %s\n" , a->nom);
+    printf ("Espèce: %s\n" , nomsEspeces[esp]);
+    printf ("Age: %d ans\n" , 2025 - a->annee); 
+
+    printf("Poids: %.2f kg\n" , a->poids);
+
+    if (a->commentaire[0] != '\0') { 
+        printf("Coommentaire: %s\n", a->commentaire);
+    } else { 
+        printf("Commentaire: Aucun \n" );
+    }
+}
