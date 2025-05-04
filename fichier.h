508d1fbaf5072efc10ac0ee4fbd1771c8b11418a
syrine-chellat <syrine.chellat@gmail.com>
2025-05-04 15:21:46 +0200

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_nom 50
 #define MAX_com 100
+#define MAX_animaux 50
+#define NB_ESPECE 4
 
 typedef enum {CHIEN, CHAT, HAMSTER, AUTRUCHE} Espece;
+
+const char* nomsEspeces[] = {"CHIEN","CHAT","HAMSTER","AUTRUCHE"};
 
 typedef Struct{
 int identifiant;
