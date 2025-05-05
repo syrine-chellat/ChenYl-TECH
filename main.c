8a1ec102462b3daa24017d27594dbeada488ee0c
syrine-chellat <syrine.chellat@gmail.com>
2025-05-05 21:11:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
 #include <fichier.h>
 #include <adopt.h>
-int main(){
+int main()
+{
+    Animal animal[50];
+    int esp;
+    for(int i=0; i<2; i++){
+	    constructeur_animal(&animal[i], &esp, i);
+	}
+    int choix = afficher_menu();
+    int cheap = 50;
 	
-	int choix = afficher_menu();
-	
-	Animal animal[MAX_animaux];
-	int esp;
-	
+
+do{
 	switch (choix){
 		case 1 :
-			for(int i=0; i<MAX_animaux; i++){
-				if(animal[i].identifiant == 0){
+			for(int i=0; i<50; i++){
+				if(animal[i].poids == 0){
 					constructeur_animal(&animal[i], &esp, i);
 					FILE* fichier = creer_fichier_animal(&animal[i], esp, i);
 					fclose(fichier);
 				}
 				break;
 			}
-		//case 2 : 
-			//rechercher_animal();
-			//afficher_animal();
-			//break;
+		case 2 : 
+			rechercher_animal(&animal[0], 50);
+			break;
 		case 3 : 
 			for(int i=0; i<MAX_animaux; i++){
 				esp = animal[i].espece;
-				afficher_animal(&animal[i], esp);
+				if(animal[i].poids > 0){  // ou autre critère d’existence
+                  afficher_animal(&animal[i], animal[i].espece);
+                }
 			}
 			break;
 		case 4 : 
+		    adopter_animal(&animal[0], &cheap);
+		    break;
 		default :
 			break;
 
 	}
+}while(choix!=5);
 
-	
-	return 0;
+
+    return 0;
 }
