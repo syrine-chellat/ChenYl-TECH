72e4c9212c9e8e098cc498c51c1bd170c2800358
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-05 16:40:26 +0200

    Update adoptanim.c

--- adoptanim.c
+++ adoptanim.c
@@ -30,7 +30,7 @@ void adopter_animal(Animal* animaux, int* nombre_animaux) {
     break;
   }
   
-  if(trouvé_id!=1){
+    if(trouvé_id!=1){
     printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n", identifiant);
   }
   
