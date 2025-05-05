501c5b33627252fb052dc972bdeb55b2822db541
syrine-chellat <syrine.chellat@gmail.com>
2025-05-05 21:08:21 +0200

    Update adoptanim.c

--- adoptanim.c
+++ adoptanim.c
@@ -32,4 +32,32 @@ void adopter_animal(Animal* animaux, int* nombre_animaux) {
 
 
 
+void adopter_animal(Animal* animaux, int* nombre_animaux) {
+  int identifiant;
+  int trouvé_id=0;
+  printf("Entrez le numéro d'identifiant de l'animal à adopter :");
+  scanf("%d",&identifiant);
 
+  
+  for (int i=0; i<*nombre_animaux;i++){
+    
+    if(animaux[i].identifiant==identifiant){
+      printf("Animal trouvé : %s(%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
+    }
+    
+    for(int j=i; j<*nombre_animaux-1; j++){
+      animaux[j]=animaux[j+1];
+    }
+  }
+    (*nombre_animaux)--;
+    
+    printf("L'animal a été adopté. Prenez bien soin de lui.\n");
+    trouvé_id=1;
+    break;
+  }
+  
+    if(trouvé_id!=1){
+    printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n", identifiant);
+  }
+  
+}
