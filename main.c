f3738a6b0ae4dc4d5d6b33d515a0a3bec55746b3
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-05 12:13:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
 #include <fichier.h>
 #include <adopt.h>
-
+int main (){
+Animal* tableau=malloc(NB_ANIMAUX_MAX * sizeof(Animal));
+  if(tableau==NULL){
+        printf("Erreur d'allocation mémoire.\n");
+        return 1;
+  }
+  rechercher_animal( tableau, NB_ANIMAUX_MAX);
+  free(tableau);
+return 0;
+}
