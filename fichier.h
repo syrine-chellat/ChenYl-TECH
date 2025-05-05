184d24e7887464f43cedc71c0e2e661e7a213812
syrine-chellat <syrine.chellat@gmail.com>
2025-05-05 20:55:42 +0200

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -5,10 +5,11 @@
 #define MAX_com 100
 #define MAX_animaux 50
 #define NB_especes 4
+#define NB_ANIMAUX_MAX 50
 
-typedef enum {CHIEN, CHAT, HAMSTER, AUTRUCHE} Espece;
+typedef enum {CHIEN, CHAT, HAMSTER, AUTRUCHE, LAPIN, LAMA} Espece;
 
-const char* nomsEspeces[] = {"CHIEN","CHAT","HAMSTER","AUTRUCHE"};
+const char* nomsEspeces[] = {"CHIEN","CHAT","HAMSTER","AUTRUCHE", "LAPIN", "LAMA"};
 
 typedef struct{
 int identifiant;
@@ -18,4 +19,3 @@ int annee;
 float poids;
 char commentaire[MAX_com];
 } Animal;
-
