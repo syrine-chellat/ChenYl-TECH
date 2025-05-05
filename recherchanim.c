1e5f820878901b5bc0c1917c028b9ada73c362fa
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-05 12:27:41 +0200

    Update recherchanim.c

--- recherchanim.c
+++ recherchanim.c
@@ -11,7 +11,10 @@ void rechercher_animal(Animal* animaux, int nombre_animaux){
   do{
     printf("Voulez vous chercher par nom ? \n1=oui \n2=non \n");
     scanf("%d",&choix);
-  }while(choix==1&&choix==2);
+    if(choix!=1&&choix!=2){
+      printf("Veuillez entrer 1 ou 2 uniquement.\n");
+    }
+  }while(choix!=1&&choix!=2);
   if(choix==1){
     printf("Entrez le nom :\n");
     scanf("%s",nom);
@@ -19,21 +22,30 @@ void rechercher_animal(Animal* animaux, int nombre_animaux){
 
 // RECHERCHER AVEC L'ESPECE
   do{
-    printf("Voulez vous chercher par espèce ? \n1=espèce \n2=non \n");
+    printf("Voulez vous chercher par espèce ? \n1=oui \n2=non \n");
     scanf("%d",&choix);
-  }while(choix==1&&choix==2);
+     if(choix!=1&&choix!=2){
+      printf("Veuillez entrer 1 ou 2 uniquement.\n");
+     }
+  }while(choix!=1&&choix!=2);
   if(choix==1){
     do{
       printf("Choisissez l'espèce : \n1-Chien \n2-Chat \n3-Hamster \n4-Autruche \n5-Lapin \n6-Lama \n");
       scanf("%d",&espece);
-    }while(choix==1&&choix==2&&choix==3&&choix==4&&choix==5&&choix==6);
+       if(choix!=1&&choix!=2){
+        printf("Veuillez entrer 1, 2, 3, 4, 5 ou 6 uniquement.\n");
+       }
+    }while(choix!=1&&choix!=2&&choix!=3&&choix!=4&&choix!=5&&choix!=6);
   }
 
 // RECHERCHER AVEC LA TRANCHE D'AGE
   do{
     printf("Voulez vous cherchez par sa tranche d'âge ? \n1=oui \n2=non ) :\n");
     scanf("%d",&choix);
-  }while(choix==1&&choix==2);
+    if(choix!=1&&choix!=2){
+      printf("Veuillez entrer 1 ou 2 uniquement.\n");
+     }
+  }while(choix!=1&&choix!=2);
   if(choix==1){
     do{
       printf("Choisissez la tranche d'âge : \n1-jeune(<2ans) \n2-senior(>10ans) :\n");
