d710719f84ed430a72406a351253589d71dee041
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-06 22:41:58 +0200

    Update and rename inventaire_nb.c to inventaire_age.c

---
+++ inventaire_age.c
@@ -0,0 +1,32 @@
+
+void INV_AGE_ASC(Animal* animaux, int nombre_animaux){
+
+  int tranches_age[4]={0};
+
+
+  for (int i=0; i<nombre_animaux; i++){
+    int age = 2025 - animaux[i].annee;
+
+    if (age>=0 && age<=2){
+      tranches_age[0]++;
+    }
+      
+    else if (age<=5){
+      tranches_age[1]++;
+    }
+      
+    else if (age<=10){
+      tranches_age[2]++;
+    }
+    else{
+      tranches_age[3]++;
+    }
+  }
+
+printf("Il y a %d dans la tranche d'âge de 0 à 2 ans.\n",tranches_age[0]);
+printf("Il y a %d dans la tranche d'âge de 3 à 5 ans.\n",tranches_age[1]);
+printf("Il y a %d dans la tranche d'âge de 6 à 10 ans.\n",tranches_age[2]);
+printf("Il y a %d dans la tranche d'âge de plus de 11 ans.\n",tranches_age[3]);
+printf("Il y a au total %d animaux.\n",nombre_animaux);
+
+}
