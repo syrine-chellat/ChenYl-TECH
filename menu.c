37bf7ced477c352425924edd655c6624d9c32e15
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-06 22:46:07 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -8,10 +8,11 @@ printf("1. Ajouter un animal \n" );
 printf("2. Rechercher un animal \n");
 printf("3. Afficher tous les animaux \n");
 printf("4. Adopter un animal \n");
-printf("5. Quitter \n");
+// printf("5.Fonctionnalités des variantes\n");
+printf("5. Quitter \n"); // printf("6.Quitter\n");
 printf("Choix: ");
  scanf("%d" , &choix); 
- while (choix < 1 || choix > 5 ) {
+ while (choix < 1 || choix > 5 ) { // while (choix < 1 || choix > 6 ) {
           printf("Erreur: Choix invalide.");
           scanf("%d" , &choix);
    }
