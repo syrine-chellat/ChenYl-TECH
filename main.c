63967da8c9c2bc7d21ec875e7064eb3bacd93f64
syrine-chellat <syrine.chellat@gmail.com>
2025-05-06 20:34:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,38 +4,48 @@ int main()
 {
     Animal animal[50];
     int esp;
+    int nbanim = compter_animaux(animal, MAX_animaux);
+    
+    for(int i=0; i<MAX_animaux; i++){
+        animal[i].identifiant = 0;
+        strncpy(animal[i].nom, "vide", MAX_nom);
+        animal[i].nom[MAX_nom - 1] = '\0';
+        animal[i].espece = VIDE;
+        animal[i].annee = 0;
+        animal[i].poids = 0;
+    }
+    
     for(int i=0; i<2; i++){
-	    constructeur_animal(&animal[i], &esp, i);
+	    constructeur_animal(animal+i, &esp, i);
 	}
-    int choix = afficher_menu();
-    int cheap = 50;
-    int nb_animaux=2;
-	
+
+	int choix;
 
 do{
+    choix = afficher_menu();
 	switch (choix){
 		case 1 :
-			for(int i=0; i<50; i++){
+			for(int i=0; i<MAX_animaux; i++){
 				if(animal[i].poids == 0){
-					constructeur_animal(&animal[i], &esp, i);
-					FILE* fichier = creer_fichier_animal(&animal[i], esp, i);
-					fclose(fichier);
+					constructeur_animal(animal+i, &esp, i);
+					//FILE* fichier = creer_fichier_animal(&animal[i], esp, i);
+					//fclose(fichier);
 				}
-				break;
 			}
+			break;
 		case 2 : 
-			rechercher_animal(&animal[0], 50); //rechercher_animal(&animal[0], nb_animaux);
+			rechercher_animal(animal, MAX_animaux);
 			break;
 		case 3 : 
 			for(int i=0; i<MAX_animaux; i++){
 				esp = animal[i].espece;
 				if(animal[i].poids > 0){  // ou autre critère d’existence
-                  afficher_animal(&animal[i], animal[i].espece);
+                  afficher_animal(animal+i, animal[i].espece);
                 }
 			}
 			break;
 		case 4 : 
-		    adopter_animal(&animal[0], &cheap);
+		    adopter_animal(animal, &nbanim);
 		    break;
 		default :
 			break;
