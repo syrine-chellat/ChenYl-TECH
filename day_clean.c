9620d7430c4a1dff762a3a020e8dac1591e89bf0
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-08 20:12:27 +0200

    Update day_clean.c

--- day_clean.c
+++ day_clean.c
@@ -1,1 +1,10 @@
+#include "fichier.h"
 
+void DAY_CLEAN(Animal* animaux, nombre_animaux){
+  // cage vide = 2 minutes/jour
+  // hamster/chat = 10 minutes/jour + 20 minutes/semaine
+  // autruche = 20 minutes/jour + 45 minutes/semaine
+  // chien = 5 minutes/jour + 20 minutes/semaine
+  // lapin = 15 minutes/jour + 15 minutes/semaine
+  // lama = 15 minutes/jour + 45 minutes/semaine
+}
