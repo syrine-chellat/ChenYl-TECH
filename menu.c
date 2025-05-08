61365a81824f7a1d5de2fde4ffb10014e575f08f
syrine-chellat <syrine.chellat@gmail.com>
2025-05-08 16:18:25 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,6 +1,5 @@
 #include <fichier.h>
 
-
 int afficher_menu() {
   int choix;
 printf("\n=== Menu ChenYI-Tech ===\n");
@@ -8,11 +7,12 @@ printf("1. Ajouter un animal \n" );
 printf("2. Rechercher un animal \n");
 printf("3. Afficher tous les animaux \n");
 printf("4. Adopter un animal \n");
+printf("5. Inventaire \n");
 // printf("5.Fonctionnalités des variantes\n");
-printf("5. Quitter \n"); // printf("6.Quitter\n");
+printf("6. Quitter \n"); // printf("6.Quitter\n");
 printf("Choix: ");
  scanf("%d" , &choix); 
- while (choix < 1 || choix > 5 ) { // while (choix < 1 || choix > 6 ) {
+ while (choix < 1 || choix > 6 ) { // while (choix < 1 || choix > 6 ) {
           printf("Erreur: Choix invalide.");
           scanf("%d" , &choix);
    }
