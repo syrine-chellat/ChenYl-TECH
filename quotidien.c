ee985ec51057817f71ec2e8ae1e95de1339c6c49
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-08 20:35:30 +0200

    Update quotidien.c

--- quotidien.c
+++ quotidien.c
@@ -22,3 +22,24 @@ float DAY_FOOD(Animal* a, int taille){
     }
     return croquette;
 }
+
+int DAY_CLEAN(Animal* a, int taille){
+    int temps = 0;
+    for (int i=0; i<taille; i++){
+        if (a[i].espece == 2 || a[i].espece == 1 ){  // hamster/chat ( 2+1 ) = 10 minutes/jour + 20 minutes/semaine
+
+        }
+        if (a[i].espece == 3 || a[i].espece == 5 ){  // autruche/lama ( 3+5 ) = 20 minutes/jour + 45 minutes/semaine
+
+        }
+        if(a[i].espece == 0 || a[i].espece == 4){    // chien/lapin ( 0+4 )= 5 minutes/jour + 20 minutes/semaine
+
+        }
+        else{                                        // cage vide = 2 minutes/jour
+
+        }
+    }
+    return temps;
+}
+
+
