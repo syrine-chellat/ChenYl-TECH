56a7df8fed655eb4b61c9c9451359c64a262cd5e
sankavi-k <sankavi.kumanan@gmail.com>
2025-05-08 21:48:49 +0200

    Update quotidien.c

--- quotidien.c
+++ quotidien.c
@@ -23,23 +23,26 @@ float DAY_FOOD(Animal* a, int taille){
     return croquette;
 }
 
-int DAY_CLEAN(Animal* a, int taille){
+void DAY_CLEAN(Animal* a, int taille){               
     int temps = 0;
     for (int i=0; i<taille; i++){
-        if (a[i].espece == 2 || a[i].espece == 1 ){  // hamster/chat ( 2+1 ) = 10 minutes/jour + 20 minutes/semaine
-
+        if (a[i].espece == 2 || a[i].espece == 1 ){  // hamster et chat = 10 minutes/jour + 20 minutes/semaine
+        temps += (10*7) + 20;
         }
-        if (a[i].espece == 3 || a[i].espece == 5 ){  // autruche/lama ( 3+5 ) = 20 minutes/jour + 45 minutes/semaine
-
+        if (a[i].espece == 3 || a[i].espece == 5 ){  // autruche et lama = 20 minutes/jour + 45 minutes/semaine
+        temps += (20*7) + 45;
         }
-        if(a[i].espece == 0 || a[i].espece == 4){    // chien/lapin ( 0+4 )= 5 minutes/jour + 20 minutes/semaine
-
+        if(a[i].espece == 0 || a[i].espece == 4){    // chien et lapin = 5 minutes/jour et 20 minutes/semaine
+        temps += (5*7) + 20;
         }
-        else{                                        // cage vide = 2 minutes/jour
-
+        else{                                        // cage vide = 5 minutes/jour
+        temps += 2*7
         }
     }
-    return temps;
+    int heures = temps/60;
+    int minutes = temps%60;
+
+    printf("Le temps total de nettoyage hebdomadaire du refuge est de : %d heures et %d minutes\n", heures, minutes); // pour convertir les minutes en heures
 }
 
 
