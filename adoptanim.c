119f4e35cffa66999c92200602d05c6298a58fd1
syrine-chellat <syrine.chellat@gmail.com>
2025-05-08 16:45:29 +0200

    Update adoptanim.c

--- adoptanim.c
+++ adoptanim.c
@@ -11,14 +11,14 @@ void adopter_animal(Animal* animaux, int* nombre_animaux) {
     
         if(animaux[i].identifiant==identifiant){
             trouve_id = 1;
+            printf("Animal trouvé : %s (%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
+            (*nombre_animaux)--;
+            printf("L'animal a été adopté. Prenez bien soin de lui.\n");
             animaux[i].identifiant = 0;
             strncpy(animaux[i].nom, "vide", MAX_nom);
             animaux[i].nom[MAX_nom - 1] = '\0';
             animaux[i].annee = 0;
             animaux[i].poids = 0;
-            printf("Animal trouvé : %s(%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
-            (*nombre_animaux)--;
-            printf("L'animal a été adopté. Prenez bien soin de lui.\n");
         }
     
     /*for(int j=i; j<*nombre_animaux-1; j++){
@@ -30,4 +30,5 @@ void adopter_animal(Animal* animaux, int* nombre_animaux) {
     if(trouve_id==0){
     printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n", identifiant);
     }
+  
 }
