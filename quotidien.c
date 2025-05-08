c7d7380c5fd9aca51606d708c44124b614ae4f11
syrine-chellat <syrine.chellat@gmail.com>
2025-05-08 20:18:43 +0200

    Update quotidien.c

--- quotidien.c
+++ quotidien.c
@@ -6,17 +6,17 @@ float DAY_FOOD(Animal* a, int taille){
         if(a[i].poids > 0){
             if(a[i].espece == 0 || a[i].espece == 1 || a[i].espece == 4){
                 if(2025 - a[i].annee < 2){
-                    croquette += 0,5;
+                    croquette += 0.5;
                 }
                 else{
-                    croquette += 0,1 * a[i].poids;
+                    croquette += 0.1 * a[i].poids;
                 }
             }
             if(a[i].espece == 2){
-                croquette += 0,02;
+                croquette += 0.02;
             }
             if(a[i].espece == 3 || a[i].espece == 5){
-                croquette += 2,5;
+                croquette += 2.5;
             }
         }
     }
