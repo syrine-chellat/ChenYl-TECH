8dafdccc9be8a84c805676a301a10da3e8e5fba6
nisrine-lk <nisrine.lakhmiri@etu.u-cergy.fr>
2025-05-08 14:39:08 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,28 +1,15 @@
 #include <fichier.h>
 
-void afficher_animal(Animal a ) {
-  printf ("\n-- Fiche Animal --\n");
-  printf ("ID: %d\n" , a.id);
-  printf ("Nom: %s\n" , a.nom);
-  printf ("Espéce: %s\n" , a.espece);
-  printf ("Age: %d ans\n" , 2025 - a.annee_naissance); 
-
-printf("Poids: %.2f kg\n" , a.poids);
-
-if (a.commentaire[0] != '\0') { 
-  printf("Coommentaire: %s\n", a.commentaire);
-} else { 
-    printf("Commentaire: Aucun \n" );
-}
-
-
 void afficher_animal(Animal* a, int esp) {
     printf ("\n-- Fiche Animal --\n");
     printf ("ID: %d\n" , a->identifiant);
     printf ("Nom: %s\n" , a->nom);
+  if (esp >= 0 && esp < NB_ESPECES) {
     printf ("Espèce: %s\n" , nomsEspeces[esp]);
+  } else { 
+    printf (" Espéce: Inconnue\n");
+  }
     printf ("Age: %d ans\n" , 2025 - a->annee); 
-
     printf("Poids: %.2f kg\n" , a->poids);
 
     if (a->commentaire[0] != '\0') { 
@@ -36,3 +23,10 @@ void afficher_animaux(Animal animaux[], int nb_animaux) {
        printf ("\n Auncun animal dans le refuge. \n");
 return;
    }
+printf ("==== Animaux du refuge ====\n ");
+for( int i = 0; i< nb_animaux; i++) {
+ if (animaux[i].identifient != -1) {
+      afficher_animal(&animaux[i], animaux[i].espece);
+ }
+}
+}
