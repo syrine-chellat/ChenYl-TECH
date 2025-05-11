36dfca4c50d62db1254aa58b4bfc734cc24764d4
syrine-chellat <syrine.chellat@gmail.com>
2025-05-11 16:44:44 +0200

    Update adoption.c

--- adoption.c
+++ adoption.c
@@ -7,28 +7,23 @@ void adopter_animal(Animal* animaux, int* nombre_animaux) {
   scanf("%d",&identifiant);
 
   
-    for (int i=0; i<*nombre_animaux;i++){      // boucle qui permet de comparer l'identifiant saisi à l'identifiant de chaque animal présent dans le refuge
+  for (int i=0; i<*nombre_animaux;i++){      // boucle qui permet de comparer l'identifiant saisi à l'identifiant de chaque animal présent dans le refuge
     
-        if(animaux[i].identifiant==identifiant){
-            trouve_id = 1;
-            printf("Animal trouvé : %s (%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
-            (*nombre_animaux)--;
-            printf("L'animal a été adopté. Prenez bien soin de lui.\n");
-            animaux[i].identifiant = 0;
-            strncpy(animaux[i].nom, "vide", MAX_nom);
-            animaux[i].nom[MAX_nom - 1] = '\0';
-            animaux[i].annee = 0;
-            animaux[i].poids = 0;
-        }
-    
-    /*for(int j=i; j<*nombre_animaux-1; j++){
-      animaux[j]=animaux[j+1];
-    }*/
-    
+    if(animaux[i].identifiant==identifiant){
+        trouve_id = 1;
+        printf("Animal trouvé : %s (%d) -- en cours de supression\n", animaux[i].nom, animaux[i].identifiant);
+        (*nombre_animaux)--;
+        printf("L'animal a été adopté. Prenez bien soin de lui.\n");
+        animaux[i].identifiant = 0;
+        strncpy(animaux[i].nom, "vide", MAX_nom);
+        animaux[i].nom[MAX_nom - 1] = '\0';
+        animaux[i].annee = 0;
+        animaux[i].poids = 0;
     }
+}
 
     if(trouve_id==0){
-    printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n", identifiant);
+        printf("Aucun animal avec cette identifiant %d n'a été trouvé. \n", identifiant);
     }
   
 }
