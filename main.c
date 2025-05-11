0878a8bbd1f478e6bfe993b0c0f53f83168ac1b5
syrine-chellat <syrine.chellat@gmail.com>
2025-05-11 17:28:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -31,86 +31,80 @@ int main()
 		lire_animaux(animal[i], i);
 	}
     
-
 	int choix;
 
     do{
-	clrscr();
-        choix = afficher_menu();
-	clrscr();
-	    switch (choix){
-		    case 1 :
-			    for(int i=0; i<MAX_animaux; i++){
-				    if(animal[i].poids == 0){
-					    constructeur_animal(animal+i, &esp, i);
-					    FILE* fichier = creer_fichier_animal(&animal[i], i);
-					    fclose(fichier);
-					    printf("L'animal a bien été enregistré, merci de nous l'avoir confié! \n");
-					     if(retour){
-			    	break;
-			    }
+		clrscr();
+        	choix = afficher_menu();
+		clrscr();
+	    	switch (choix){
+		   		case 1 :  //ajouter
+			    	for(int i=0; i<MAX_animaux; i++){
+				    	if(animal[i].poids == 0){
+					    	constructeur_animal(animal+i, &esp, i);
+					    	FILE* fichier = creer_fichier_animal(&animal[i], i);
+					    	fclose(fichier);
+					    	printf("L'animal a bien été enregistré, merci de nous l'avoir confié! \n");
+							retour = retour_menu();
+					     	if(retour){
+			    				break;
+			   				 }
 					    	clrscr();
-					    break;
-				    }
-			    }
-			    retour = retour_menu();
-			    if(retour){
-			    	break;
-			    }
-			    	clrscr();
-		    	break;
-		    case 2 : 
-			    rechercher_animal(animal, MAX_animaux);
-			    retour = retour_menu();
+					    	break;
+				    	}
+			    	}
+		    	case 2 : //rechercher
+			    	rechercher_animal(animal, MAX_animaux);
+			    	retour = retour_menu();
 				    if(retour){
-				        break;
+				    	break;
 					}
 			    	clrscr();
-		    break;
-	    	case 3 : 
-		    	for(int i=0; i<MAX_animaux; i++){
-			    	esp = animal[i].espece;
-			    	if(animal[i].poids > 0){  
-                    afficher_animal(animal+i, animal[i].espece);
-                    }
-		    	}
-		        retour = retour_menu();
-				if(retour){
-				    break;
-				}
-				clrscr();
-		    break;
-	       	case 4 : 
-		         adopter_animal(animal, &nbanim);
-		         retour = retour_menu();
-			   	if(retour){
-			   	    break;
-				}
-				clrscr();
-		    break;
-		    case 5 :
-		        compter_espece(animal, MAX_animaux, &compte, 6);
-	            INV_NB_DESC(animal, &compte, 6);
-	            INV_AGE_ASC(animal, MAX_animaux);
-		    	retour = retour_menu();
-			   	if(retour){
-			   	    break;
-				}
-			    	clrscr();
-		    break;
-			case 6 :
-			    croquette = DAY_FOOD(animal, MAX_animaux);
-			    printf("La quantité de croquette quotidienne à prévoir est %f kg \n", croquette);
-			    DAY_CLEAN(animal, MAX_animaux);
-			    retour = retour_menu();
-				if(retour){
-				    break;
-				}
+		    		break;
+	    		case 3 : //afficher
+		    		for(int i=0; i<MAX_animaux; i++){
+			    		esp = animal[i].espece;
+			    		if(animal[i].poids > 0){  
+                    		afficher_animal(animal+i, animal[i].espece);
+                    	}
+		    		}
+		       		retour = retour_menu();
+					if(retour){
+				    	break;
+					}
 					clrscr();
-		    break;
-    	}
-    }while(choix!=7);
-
+		    		break;
+	       		case 4 :  //adopter
+		        	adopter_animal(animal, &nbanim);
+		        	retour = retour_menu();
+			   		if(retour){
+			   	    	break;
+					}
+					clrscr();
+		    		break;
+		    	case 5 :  //inventaire
+		       		compter_espece(animal, MAX_animaux, &compte, 6);
+	            	INV_NB_DESC(animal, &compte, 6);
+	            	INV_AGE_ASC(animal, MAX_animaux);
+		    		retour = retour_menu();
+			   		if(retour){
+			   	    	break;
+					}
+			    	clrscr();
+		    		break;
+				case 6 :  //gestion du quotidien
+			    	croquette = DAY_FOOD(animal, MAX_animaux);
+			    	printf("La quantité de croquette quotidienne à prévoir est %f kg \n", croquette);
+			    	DAY_CLEAN(animal, MAX_animaux);
+			    	retour = retour_menu();
+					if(retour){
+				    	break;
+					}
+					clrscr();
+		    		break;
+    		}
+    }while(choix!=7); 
+	//quitter
 
     return 0;
 }
