60fe18185b6dcd170479e602dfffb06d78bcf7e6
syrine-chellat <syrine.chellat@gmail.com>
2025-05-11 16:40:03 +0200

    Update ajout.c

--- ajout.c
+++ ajout.c
@@ -9,59 +9,59 @@ void constructeur_animal(Animal* a, int* esp, int n){
     strncpy(a->commentaire, "vide", MAX_com);
     a->commentaire[MAX_com - 1] = '\0';   //met '\0' à la fin si la chaîne est trop longue
     
-  int verif;
-  printf("Nom animal : ");
-  scanf("%s", a->nom);
+    int verif;
+    printf("Nom animal : ");
+    scanf("%s", a->nom);
 
-  do{
-    printf("Saisir le numéro de l'espèce : \n 0-Chien \n 1-Chat \n 2-Hamster \n 3-Autruche \n 4-Lapin \n 5-Lama \n");
-    verif = scanf("%d", esp);
-    vide_buffer();
-  } while(*esp<0 || *esp>5 || verif!=1);
-  a->espece = (Espece)*esp;
+    do{
+      printf("Saisir le numéro de l'espèce : \n 0-Chien \n 1-Chat \n 2-Hamster \n 3-Autruche \n 4-Lapin \n 5-Lama \n");
+      verif = scanf("%d", esp);
+      vide_buffer();
+    } while(*esp<0 || *esp>5 || verif!=1);
+    a->espece = (Espece)*esp;
 
-  do{
-    printf("année de naissance : ");
-    verif = scanf("%d", &a->annee);
-    vide_buffer();
-  } while(a->annee<1900 || a->annee>2025 || verif!=1);
+    do{
+      printf("année de naissance : ");
+      verif = scanf("%d", &a->annee);
+      vide_buffer();
+    } while(a->annee<1900 || a->annee>2025 || verif!=1);
   
  
-  do{  
-    printf("Poids : ");
-    verif = scanf("%f", &a->poids);
-    vide_buffer();
-  } while(a->poids<0 || a->poids>400 || verif!=1);
+    do{  
+      printf("Poids : ");
+      verif = scanf("%f", &a->poids);
+      vide_buffer();
+    } while(a->poids<0 || a->poids>400 || verif!=1);
 
-  int com;
-  do{
-    printf("Voulez-vous saisir un commentaire ? Si oui, taper 1, sinon taper 0 \n");
-    verif = scanf("%d", &com);
-    vide_buffer();
+    int com;
+    do{
+      printf("Voulez-vous saisir un commentaire ? Si oui, taper 1, sinon taper 0 \n");
+      verif = scanf("%d", &com);
+      vide_buffer();
    
-} while((com!=1 && com!=0)||(verif!=1));
+    } while((com!=1 && com!=0)||(verif!=1));
 
 
-  if(com){
-    printf("Commentaire : ");
-    if (fgets(a->commentaire, MAX_com, stdin) == NULL) {
+    if(com){
+      printf("Commentaire : ");
+      if (fgets(a->commentaire, MAX_com, stdin) == NULL) {
         printf("erreur lecture commentaire");
         exit(2);
-    }
-    else{
+      }
+      else{
         int len = strlen(a->commentaire);
-        if (len > 0 && a->commentaire[len - 1] == '\n') {   //vérifier s’il y a un \n à la fin de la chaîne
+          if (len > 0 && a->commentaire[len - 1] == '\n') {   //vérifier s’il y a un \n à la fin de la chaîne
             a->commentaire[len - 1] = '\0';    //le remplacer par \0
-        }
+          }
+      }
     }
-  }
     
-  else{
-    strncpy(a->commentaire, "vide", MAX_com);
-    a->commentaire[MAX_com - 1] = '\0'; //mettre le commentaire à "vide" si l'utilisateur ne veut pas en mettre
-  }
+    else{
+      strncpy(a->commentaire, "vide", MAX_com);
+      a->commentaire[MAX_com - 1] = '\0'; //mettre le commentaire à "vide" si l'utilisateur ne veut pas en mettre
+    }
 
-  a->identifiant = n; 
+    a->identifiant = n; 
 }
 
 
